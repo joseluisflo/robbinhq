7c6a82cb7b44f5034fe4eaba1d403ed0cc345cb1
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-24 08:38:53 +0000

    antes de quiero quites esto en dashboard este titulo

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -30,10 +30,6 @@ const data = [
 export default function DashboardPage() {
   return (
     <div className="space-y-8">
-      <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-      </div>
-
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
