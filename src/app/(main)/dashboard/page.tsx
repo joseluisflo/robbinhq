ef07aa03bf3cb76c47927c05a033847e210770e5
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-26 23:04:26 +0000

    okay perfecto antes de continuar vamos a desarrollar nuestro dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -17,6 +17,7 @@ import {
   Tooltip,
 } from 'recharts';
 import { Bot, CheckCircle, Clock } from 'lucide-react';
+import { useUser } from '@/firebase';
 
 const data = [
   { name: 'Jan', tasks: 30 },
@@ -28,8 +29,16 @@ const data = [
 ];
 
 export default function DashboardPage() {
+  const { user } = useUser();
+
   return (
     <div className="space-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Hi {user?.displayName || 'there'}</h2>
+        <p className="text-muted-foreground">
+          here's a quick overview of what's happned in the last 7 days.
+        </p>
+      </div>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
