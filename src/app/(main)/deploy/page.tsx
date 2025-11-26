be28cfc240906809df4524936c636dccb4ca312e
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-26 21:26:36 +0000

    vamos en deploy remueve este boton accion ya que tenemos un boton settup

--- src/app/(main)/deploy/page.tsx
+++ src/app/(main)/deploy/page.tsx
@@ -4,9 +4,8 @@ import {
   CardContent,
 } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
-import { MessageCircle, Mail, Phone, MoreHorizontal } from 'lucide-react'
+import { MessageCircle, Mail, Phone } from 'lucide-react'
 import Link from 'next/link'
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu'
 
 const channels = [
   {
@@ -54,20 +53,10 @@ export default function DeployPage() {
                   </p>
                 </Link>
               </CardContent>
-            <CardFooter className="p-4 flex items-center justify-between gap-2">
-               <Button variant="outline" asChild className="flex-1">
+            <CardFooter className="p-4">
+               <Button variant="outline" asChild className="w-full">
                  <Link href={channel.href}>Setup</Link>
                </Button>
-               <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" size="icon" className="h-8 w-8 shrink-0">
-                    <MoreHorizontal className="h-4 w-4" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem>Configure</DropdownMenuItem>
-                </DropdownMenuContent>
-               </DropdownMenu>
             </CardFooter>
           </Card>
         ))}
