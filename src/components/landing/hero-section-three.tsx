12b522948835854f95e0a895e97a89a428dd848a
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-29 05:23:54 +0000

    vamso con este Customer Support, Handled Automatically

--- src/components/landing/hero-section-three.tsx
+++ src/components/landing/hero-section-three.tsx
@@ -13,7 +13,7 @@ export default function HeroSection() {
                     <div className="relative pt-32 pb-24">
                         <div className="mx-auto max-w-5xl px-6">
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-                                <h1 className="text-5xl font-bold lg:text-6xl leading-normal">Build 10x Faster with Mist</h1>
+                                <h1 className="text-5xl font-bold lg:text-6xl leading-normal">Customer Support, Handled Automatically</h1>
                                 <div className="space-y-6">
                                     <p className="text-xl">Craft. Build. Ship Modern Websites With AI Support.</p>
                                     <div className="grid sm:flex items-start gap-3">
