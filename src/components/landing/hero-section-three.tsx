264e1349553e67c98f06fee8502d15fdd5aaa011
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-29 04:11:31 +0000

    podemos bajarlo el encabezado a 3xl y 4xl en panatallas grandes y el sub

--- src/components/landing/hero-section-three.tsx
+++ src/components/landing/hero-section-three.tsx
@@ -13,8 +13,8 @@ export default function HeroSection() {
                     <div className="relative py-24">
                         <div className="mx-auto max-w-5xl px-6">
                             <div>
-                                <h1 className="mt-8 max-w-2xl text-balance text-5xl font-bold lg:text-6xl">Build 10x Faster with Mist</h1>
-                                <p className="text-foreground my-6 max-w-2xl text-balance text-2xl">Craft. Build. Ship Modern Websites With AI Support.</p>
+                                <h1 className="mt-8 max-w-2xl text-balance text-3xl font-bold lg:text-4xl">Build 10x Faster with Mist</h1>
+                                <p className="text-foreground my-6 max-w-2xl text-balance text-xl">Craft. Build. Ship Modern Websites With AI Support.</p>
 
                                 <div className="flex flex-col items-center gap-3 *:w-full sm:flex-row sm:*:w-fit">
                                     <Button
