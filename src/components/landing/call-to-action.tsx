258c16a9a62bbf529228b5f070a07d4f33754875
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-29 18:25:21 +0000

    haz lo mismo con el cta

--- src/components/landing/call-to-action.tsx
+++ src/components/landing/call-to-action.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function CallToAction() {
     return (
-        <section className="py-16 md:py-32">
+        <section className="bg-muted/50 py-16 md:py-32">
             <div className="mx-auto max-w-5xl px-6">
                 <div className="text-center">
                     <h2 className="text-balance text-4xl font-medium lg:text-5xl">Start Building</h2>
