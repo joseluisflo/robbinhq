8efb50e9f2974dca3a07cae3a8d9fc6b930c338e
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-30 18:25:58 +0000

    okay cambia la imajen por este https://files.tryrobbin.com/assets/meetin

--- src/components/landing/content-3.tsx
+++ src/components/landing/content-3.tsx
@@ -8,7 +8,7 @@ export default function ContentSection() {
             <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-12">
                 <img
                     className="rounded-[var(--radius)] grayscale"
-                    src="https://images.unsplash.com/photo-1530099486328-e021101a494a?q=80&w=2747&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    src="https://files.tryrobbin.com/assets/meeting-people.avif"
                     alt="team image"
                     height=""
                     width=""
