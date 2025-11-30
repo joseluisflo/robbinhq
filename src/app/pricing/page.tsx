1e40891de24b9493f5f70c1da283f249d89597ee
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-11-30 19:29:46 +0000

    estructura el pricing page con estos componentes Entendido. Primero, ins

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,9 +1,14 @@
-
 'use client';
 
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { HeroHeader } from '@/components/landing/header';
+import PricingSectionTwo from '@/components/landing/pricing-section-two';
+import PricingComparatorOne from '@/components/landing/pricing-comparator-one';
+import FAQs from '@/components/landing/faqs-section-two';
+import CallToAction from '@/components/landing/call-to-action';
+import Footer from '@/components/landing/footer';
+
 
 export default function PricingPage() {
   return (
@@ -20,7 +25,12 @@ export default function PricingPage() {
             </p>
           </div>
         </section>
+        <PricingSectionTwo />
+        <PricingComparatorOne />
+        <FAQs />
+        <CallToAction />
       </main>
+      <Footer />
     </>
   );
 }
