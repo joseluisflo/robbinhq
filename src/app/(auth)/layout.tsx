ff0c1fe9e16a0c97fc203bd93c06026061ddd2d4
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-12-02 16:48:18 +0000

    en nuestro login sign up podemos poner el bg gray ese mismo que tenemos

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-muted/50 p-4">
       <div className="absolute top-8">
         <Link href="/">
           <Logo />
