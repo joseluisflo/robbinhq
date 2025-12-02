2af3067d9cfec71b1cd5cbd0c7ca60c1419b7473
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-12-02 04:26:36 +0000

    revisa y analiza nuestros items en el navbar y verifica por que aun no e

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -7,11 +7,13 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 import { cn } from '@/lib/utils'
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu"
 
 
 const menuItems = [
@@ -63,30 +65,38 @@ export const HeroHeader = () => {
                                                 variant="ghost"
                                                 size="sm">
                                                 <Link
-                                                    href={item.href}
-                                                    className="text-base">
+                                                    href={item.href}>
                                                     <span>{item.name}</span>
                                                 </Link>
                                             </Button>
                                         </li>
                                     ))}
                                     <li>
-                                        <DropdownMenu>
-                                            <DropdownMenuTrigger asChild>
-                                                <Button
-                                                    variant="ghost"
-                                                    size="sm"
-                                                    className="text-base">
-                                                    <span>Resources</span>
-                                                </Button>
-                                            </DropdownMenuTrigger>
-                                            <DropdownMenuContent>
-                                                <DropdownMenuItem>Item 1</DropdownMenuItem>
-                                                <DropdownMenuItem>Item 2</DropdownMenuItem>
-                                                <DropdownMenuItem>Item 3</DropdownMenuItem>
-                                                <DropdownMenuItem>Item 4</DropdownMenuItem>
-                                            </DropdownMenuContent>
-                                        </DropdownMenu>
+                                        <NavigationMenu>
+                                            <NavigationMenuList>
+                                                <NavigationMenuItem>
+                                                    <NavigationMenuTrigger>
+                                                        Resources
+                                                    </NavigationMenuTrigger>
+                                                    <NavigationMenuContent>
+                                                        <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                                                            <ListItem href="#" title="Item 1">
+                                                                Description for item 1.
+                                                            </ListItem>
+                                                            <ListItem href="#" title="Item 2">
+                                                                Description for item 2.
+                                                            </ListItem>
+                                                            <ListItem href="#" title="Item 3">
+                                                                Description for item 3.
+                                                            </ListItem>
+                                                            <ListItem href="#" title="Item 4">
+                                                                Description for item 4.
+                                                            </ListItem>
+                                                        </ul>
+                                                    </NavigationMenuContent>
+                                                </NavigationMenuItem>
+                                            </NavigationMenuList>
+                                        </NavigationMenu>
                                     </li>
                                 </ul>
                             </div>
@@ -147,3 +157,29 @@ export const HeroHeader = () => {
         </header>
     )
 }
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
