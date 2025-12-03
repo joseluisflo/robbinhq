fba9231fdcaa0f944eee02b178414e6ba023f322
Jose Luis Flores <jlfloressanchez01@gmail.com>
2025-12-03 21:38:11 +0000

    hagamos esto de la escala

--- src/components/landing/hero-section-three.tsx
+++ src/components/landing/hero-section-three.tsx
@@ -23,7 +23,7 @@ export default function HeroSection() {
                                       "url('https://assets.tryrobbin.com/assets/home-masthead-bg.webp')",
                                   }}
                                 >
-                                    <div className="bg-background rounded-md relative mx-auto overflow-hidden border border-transparent shadow-lg shadow-black/10 ring-1 ring-black/10">
+                                    <div className="bg-background rounded-md relative mx-auto overflow-hidden border border-transparent shadow-lg shadow-black/10 ring-1 ring-black/10 transform scale-[0.8] origin-top">
                                         <MockupHeroTraining />
                                     </div>
                                 </div>
