60581b6c2216e450d0285e074d4e34c8fa9153bf
Jose Luis Flores <jlfloressanchez01@gmail.com>
2026-01-04 22:08:46 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: DialogT

--- src/components/settings/change-plan-dialog.tsx
+++ src/components/settings/change-plan-dialog.tsx
@@ -14,6 +14,7 @@ import {
   DialogTitle,
   DialogBody,
   DialogFooter,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { CheckoutForm } from "./checkout-form";
